96b194ad3c0efe1d600a30871d6041c750388f8b
Nikhil Kumar <76116523+theproficientdev@users.noreply.github.com>
2022-02-14 17:25:12 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -8,32 +8,30 @@ class Solution{
 public:
     int *findTwoElement(int *arr, int n) {
         // code here
-        int *ans = new int[2];
-
-        set<int> s;
-        int duc = -1;
-        for (int i = 0; i < n; i++)
-        {
-            if (s.find(arr[i]) != s.end())
-            {
-                duc = arr[i];
-            }
-    
-            s.insert(arr[i]);
-        }
-    
-        int miss = -1;
-        for (int i = 1; i <= n; i++)
-        {
-            if (s.find(i) == s.end())
-            {
-                miss = i;
-                break;
-            }
-        }
-        ans[0] = duc;
-        ans[1] = miss;
-        return ans;
+        int prr[n+1];
+       fill(prr,prr+n+1,0);
+       for(int i=0;i<n;i++)
+       {
+           prr[arr[i]]+=1;
+       }
+       int rep;
+       int mis;
+       for(int i=1;i<n+1;i++)
+       {
+           if(prr[i]==2)
+           {
+               rep=i;
+           }
+           else if(prr[i]==0)
+           {
+               mis=i;
+           }
+       }
+       int *rr;
+       rr=new int[2];
+       rr[0]=rep;
+       rr[1]=mis;
+       return rr;
     }
 };
 
