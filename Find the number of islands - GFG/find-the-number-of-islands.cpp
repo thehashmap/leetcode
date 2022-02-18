8df91ac1ccb8536b88d16f2322a0cb614b89e010
Nikhil Kumar <76116523+theproficientdev@users.noreply.github.com>
2022-02-19 00:04:46 +0530

    Added solution - LeetHub

---
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+private:
+    int dx[8] = {1, 0, -1, 0, 1, -1, 1, -1};
+    int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+private:
+    void dfs(vector<vector<char>>& grid, int i, int j, int n, int m) {
+        if(i < 0 or j < 0 or i >= n or j >= m or grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        for(int k = 0; k < 8; k++) {
+            dfs(grid, i + dx[k], j + dy[k], n, m);
+        }
+    }
+public:
+    // Function to find the number of islands.
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int comps = 0;
+        int n = grid.size(), m = grid[0].size();
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == '1') {
+                    dfs(grid, i, j, n, m);
+                    comps++;
+                }
+            }
+        }
+        
+        return comps;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> grid(n, vector<char>(m, '#'));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.numIslands(grid);
+        cout << ans << '\n';
+    }
+    return 0;
+}  // } Driver Code Ends
