49b576efc178576a9ce2f5e224a5221b4d1dbcc4
Nikhil Kumar <76116523+theproficientdev@users.noreply.github.com>
2022-05-18 20:24:08 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (25.26%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int start = 0 , mid = 0 , end = nums.size() - 1;
+        // 1: Count the number of 0s, 1s and 2s and place them accordingly
+        // Two iterations required
+        
+        // Three pointers
+        int start = 0, mid = 0, end = nums.size() - 1;
         while(mid <= end){
             if(nums[mid] == 2){
-                swap(nums[mid] , nums[end]);
-                end--;
+                swap(nums[mid], nums[end--]);
+            }
+            else if(nums[mid] == 0){
+                swap(nums[mid++], nums[start++]);
             }
             else if(nums[mid] == 1) mid++;
-            else if(nums[mid] == 0){
-                swap(nums[mid] , nums[start]);
-                mid++;
-                start++;
-            }
         }
     }
 };
