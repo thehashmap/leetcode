bc7920d430ebf1feb334860eda52fbe7d4af1aa3
Nikhil Kumar <76116523+theproficientdev@users.noreply.github.com>
2022-05-19 20:40:39 +0530

    Added solution - LeetHub

--- Transform String - GFG/transform-string.cpp
+++ Transform String - GFG/transform-string.cpp
@@ -11,19 +11,17 @@ class Solution
     int transform (string A, string B)
     {
         //code here.
-        int ans=0;
-        int i=B.size()-1;
         if(A.size() != B.size()) return -1;
-        for(int j=A.size()-1;j>=0;j--){
+        int i = A.size() - 1, ans = 0;
+        for(int j = B.size() - 1; j >= 0; j--){
             if(A[j] != B[i]){
                 ans++;
-            }else{
-                i--;
             }
+            else i--;
         }
-        sort(A.begin(),A.end());
-        sort(B.begin(),B.end());
-        if(A!=B) return -1;
+        sort(A.begin(), A.end());
+        sort(B.begin(), B.end());
+        if(A != B) return -1;
         return ans;
     }
 };
