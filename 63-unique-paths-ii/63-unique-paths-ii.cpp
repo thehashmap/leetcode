a6cf9de1474f59b130e93be501586e48ff974c54
Nikhil Kumar <76116523+theproficientdev@users.noreply.github.com>
2022-05-20 09:56:18 +0530

    Time: 6 ms (36.45%), Space: 7.8 MB (41.40%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& o) {
-        int m = o.size(), n = o[0].size();
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
-        return path(o, m - 1, n - 1, dp);
+    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+        int m = Grid.size(), n = Grid[0].size();
+        vector<vector<int>> dp(m , vector<int> (n , -1));
+        return path(Grid, m - 1, n - 1, dp);
     }
-    int path(vector<vector<int>>& o, int m, int n, vector<vector<int>>& dp){
-        if(m >= 0 && n >= 0 && o[m][n] == 1) return 0;
+    
+    int path(vector<vector<int>>& Grid, int m, int n, vector<vector<int>>& dp) {
+        if(m < 0 || n < 0 || Grid[m][n] == 1) return 0;
         if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
         if(dp[m][n] != -1) return dp[m][n];
-        int up = path(o, m - 1, n, dp);
-        int left = path(o, m, n - 1, dp);
+        int up = path(Grid, m - 1, n , dp);
+        int left = path(Grid, m , n - 1, dp);
         return dp[m][n] = up + left;
     }
 };
