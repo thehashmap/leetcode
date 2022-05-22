75072581b3aa2530b030782ab17c6c121c9969c1
Nikhil Kumar <76116523+theproficientdev@users.noreply.github.com>
2022-05-22 10:54:05 +0530

    Added solution - LeetHub

---
+++ Find an Replace in String - GFG/find-an-replace-in-string.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    string findAndReplace(string S ,int Q, int index[], string sources[], string targets[]) {
+        // code here
+        string str=S;
+        int idx=0;
+        for(int i=0;i<Q;i++){
+            int ind=index[i];
+            string src=sources[i];
+            if(S.substr(ind,src.length())==src){
+                str.replace(ind+idx,src.length(),targets[i]);
+                idx+=targets[i].length()-src.length();
+            }
+        }
+        return str;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin>>S;
+        
+        int Q;
+        cin>>Q;
+        int index[Q];
+        string sources[Q], targets[Q];
+        
+        for(int i=0; i<Q; i++)
+            cin>>index[i];
+        for(int i=0; i<Q; i++)
+            cin>>sources[i];
+        for(int i=0; i<Q; i++)
+            cin>>targets[i];
+
+        Solution ob;
+        cout<<ob.findAndReplace(S,Q,index,sources,targets)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
